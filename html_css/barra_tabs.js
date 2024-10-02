bd5b634dea381fcf1162edec9d85b8bfd961e14a
SERGIO LOPES JUNIOR <sergiolj@gmail.com>
2024-10-02 15:06:43 -0300

    HTML Block e Inline concepts

--- html_css/barra_tabs.js
+++
@@ -1,8 +0,0 @@
-function abreCidade(nomeCidade){
-    var i;
-    var x=document.getElementsByClassName("cidade");
-    for(i=0;i<x.length;i++){
-        x[i].style.display="none";
-    }
-    document.getElementById(nomeCidade).style.display="block";
-}
